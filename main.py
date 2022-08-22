b34ed0aacaefc66309f73cc98c0377f5dcf8f7ab
arash_vsh <arash.vashagh@gmail.com>
2022-08-22 10:01:44 +0430

    Added Progress Report

--- main.py
+++ main.py
@@ -14,21 +14,20 @@ def load_ucr(dataset: str):
 
 
 def loadDatasets():
-    numberOfLoaeded: int = 0
+    datasetNumber: int = 1
     all_ucr_dataset_names: list[str] = ucr.list_datasets()
+    totalCountOfDatasets: int = len(all_ucr_dataset_names)
     for datasetName in all_ucr_dataset_names:
-        if numberOfLoaeded == 98: # I think it's corrupted, so skip it
-            numberOfLoaeded += 1
-            continue
         try:
             X_shape = load_ucr(datasetName)
         except:
+            print("Progress: {}/{} failed".format(datasetNumber, totalCountOfDatasets))
+            datasetNumber += 1
             continue
-        dataset = DataSet(datasetName, X_shape)
-        dataset.addToFinalList()
-        numberOfLoaeded += 1
-        print("Progress: ", numberOfLoaeded, ' loaded')
-    return len(all_ucr_dataset_names), len(ACCEPTED_DATASETS),
+        DataSet(datasetName, X_shape).addToFinalList()
+        print("Progress: {}/{} loaded".format(datasetNumber, totalCountOfDatasets))
+        datasetNumber += 1
+    return totalCountOfDatasets, len(ACCEPTED_DATASETS),
 
 
 def printResult(allDatasetsCnt: int, acceptedDatasetsCnt: int):
@@ -40,7 +39,7 @@ def printResult(allDatasetsCnt: int, acceptedDatasetsCnt: int):
 if __name__ == "__main__":
     allDatasetsCnt, acceptedDatasetsCnt = loadDatasets()
     printResult(allDatasetsCnt, acceptedDatasetsCnt)
-    with open("datasetInfo.txt", "a+") as file_object:
+    with open("datasetInfo.txt", "w") as file_object:
         for i in range(acceptedDatasetsCnt):
             file_object.write(str(i + 1) + '\n' +
                               ACCEPTED_DATASETS[i].__str__())
